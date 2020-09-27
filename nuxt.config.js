c78762df1ffe5000ca0f9b12976d3aef9c6385ce
Leo <leosc86@gmail.com>
2020-09-27 14:21:12 -0300

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,5 +31,9 @@ export default {
   modules: ["@nuxtjs/axios"],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {}
+  build: {},
+
+  server: {
+    port: process.env.PORT || 8000
+  }
 };
